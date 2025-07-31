0d99a5f005da90fd8128e8ea55ac2b36d6e60654
JcElectronic <61955883+johancass@users.noreply.github.com>
2025-07-31 08:01:43 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
-// db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // 🔐 Requerido por Neon
-  }
+  ssl: { rejectUnauthorized: false }
 });
+
+module.exports = pool;
