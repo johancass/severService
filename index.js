9664a6c5792f2bd3ab71cf604ebfa1d22242174c
JcElectronic <61955883+johancass@users.noreply.github.com>
2025-07-31 04:29:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,43 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Servidor activo. Usa POST /guardar_pago para enviar datos.');
 });
+
+app.post('/crear_pago', async (req, res) => {
+  const { id_pago, valor, estado } = req.body;
+
+  if (!id_pago || !valor || !estado) {
+    return res.status(400).json({ error: 'Faltan datos' });
+  }
+
+  try {
+    // ENVÍA los datos al script PHP de FreeHosting
+    const response = await fetch('https://jcmanosenresina.unaux.com/guardar_pago.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        id_pago,
+        valor,
+        estado
+      })
+    });
+
+    const respuesta = await response.text();
+
+    console.log('Respuesta desde FreeHosting:', respuesta);
+
+    res.status(200).json({
+      exito: true,
+      mensaje: 'Datos reenviados correctamente al servidor FreeHosting',
+      respuesta_php: respuesta
+    });
+
+  } catch (error) {
+    console.error('Error al reenviar datos:', error.message);
+    res.status(500).json({ error: 'Error al enviar datos a FreeHosting' });
+  }
+});
 // Ruta para recibir los datos del ESP32
 app.get('/crear_pago', (req, res) => {
    res.send('Servidor activo. Usa /crear_pagopara enviar datos.');
