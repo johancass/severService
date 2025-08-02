319c756df8e7c53f49f54a07dc2af86865b0ba69
JcElectronic <61955883+johancass@users.noreply.github.com>
2025-08-02 02:42:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 
 const express = require('express');
 const pool = require('./db');
-const axios = require('axios');
-cconst cors = require('cors');
-onst crypto = require('crypto');
 const app = express();
 
 
-app.use(cors()); // Permitir cualquier origen temporalmente
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // ✅ para datos tipo formulario
 
@@ -76,91 +72,6 @@ app.get('/estado_pago', async (req, res) => {
     });
   }
 });
-// server.js
-
-
-app.post('/generar_pago', async (req, res) => {
-  const { referenceCode, amount } = req.body;
-
-  const apiKey = '4Vj8eK4rloUd272L48hsrarnUA';
-  const merchantId = '508029';
-  const accountId = '512321';
-  const currency = 'COP';
-
-  const signatureString = `${apiKey}~${merchantId}~${referenceCode}~${amount}~${currency}`;
-  const signature = crypto.createHash('md5').update(signatureString).digest('hex');
-
-  const payload = {
-    language: 'es',
-    command: 'SUBMIT_TRANSACTION',
-    merchant: {
-      apiKey,
-      apiLogin: 'pRRXKOl8ikMmt9u',
-    },
-    transaction: {
-      order: {
-        accountId,
-        referenceCode,
-        description: 'Prueba desde PHP',
-        language: 'es',
-        signature,
-        notifyUrl: 'https://jcmanosenresina.unaux.com/verificar.php',
-        additionalValues: {
-          TX_VALUE: {
-            value: amount,
-            currency
-          }
-        },
-        buyer: {
-          fullName: 'Cliente Test',
-          emailAddress: 'cliente@test.com',
-          contactPhone: '3000000000',
-          dniNumber: '123456789',
-          shippingAddress: {
-            street1: 'Calle 1',
-            city: 'Bogotá',
-            state: 'Bogotá',
-            country: 'CO',
-            phone: '3000000000'
-          }
-        }
-      },
-      payer: {
-        fullName: 'Cliente Test',
-        emailAddress: 'cliente@test.com',
-        contactPhone: '3000000000',
-        dniNumber: '123456789',
-        billingAddress: {
-          street1: 'Calle 1',
-          city: 'Bogotá',
-          state: 'Bogotá',
-          country: 'CO',
-          phone: '3000000000'
-        }
-      },
-      type: 'AUTHORIZATION_AND_CAPTURE',
-      paymentMethod: 'NEQUI',
-      paymentCountry: 'CO',
-      deviceSessionId: 'abc123',
-      ipAddress: '127.0.0.1',
-      cookie: 'cookie123456',
-      userAgent: 'Mozilla/5.0'
-    },
-    test: true
-  };
-
-  try {
-    const response = await axios.post(
-      'https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi',
-      payload,
-      { headers: { 'Content-Type': 'application/json' } }
-    );
-    res.json(response.data);
-  } catch (err) {
-    res.status(500).json({ error: err.message, detalle: err.response?.data });
-  }
-});
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
